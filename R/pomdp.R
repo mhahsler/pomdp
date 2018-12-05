ba645dafefa8f2dba12e08b232e876ea3d0f296a
Michael Hahsler <michael@hahsler.net>
2018-12-05 14:07:06 -0600

    cleaned code.

--- R/pomdp.R
+++ R/pomdp.R
@@ -27,12 +27,14 @@ pomdp <- function(discount = 0,
   # grid_size is an integer
   
   ### model
-  model <- list(discount = discount, states = states , actions = actions , observations = observations , start = start , transition_prob = transition_prob , observation_prob = observation_prob , reward = reward)
+  model <- list(discount = discount, states = states, actions = actions, 
+    observations = observations, start = start, transition_prob = transition_prob,
+    observation_prob = observation_prob, reward = reward)
   
   
   ### POMDP file
   code <- character()
-  code <-  paste(c("discount:",discount, "\n") , collapse = " ")
+  code <-  paste(c("discount:", discount, "\n") , collapse = " ")
   
   # deal with rewards or costs
   values <- match.arg(values, choices = c("reward", "cost"))
@@ -41,42 +43,42 @@ pomdp <- function(discount = 0,
   # counting the number of states
   number_of_states <- length(states)
   
-  code <- paste(c(code,"states:",states, "\n"), collapse = " ")
+  code <- paste(c(code,"states:", states, "\n"), collapse = " ")
   
   # counting the number of actions
   number_of_actions <- length(actions)
   
-  code <- paste(c(code,"actions:",actions, "\n"), collapse = " ")
+  code <- paste(c(code,"actions:", actions, "\n"), collapse = " ")
   
   # counting the number of observations
   number_of_observations <- length(observations)
   
-  code <- paste(c(code,"observations:",observations, "\n"), collapse = " ")
+  code <- paste(c(code,"observations:", observations, "\n"), collapse = " ")
   
   ### starting beliefs
   
   ## if the starting beliefs are given by enumerating the probabilities for each state
   if (!is.character(start)) {
     if (sum(start)==1) {
-      code <- paste(c(code,"start:",start, "\n"), collapse = " ")
+      code <- paste(c(code,"start:", start, "\n"), collapse = " ")
     }
   }
   ## if the starting beliefs are given by a uniform distribution over all states
   if (sum(start== "uniform")==1) {
-    code <- paste(c(code,"start:",start, "\n"), collapse = " ")
+    code <- paste(c(code,"start:", start, "\n"), collapse = " ")
   } else if (start[1]!="-") {  ## if the starting beliefs include a specific subset of states
     # if the starting beliefs are given by a uniform distribution over a subset of states (using their names)
-    if (!is.na(sum(match(start , states)))) {
-      code <- paste(c(code,"start include:",start, "\n"), collapse = " ")
+    if (!is.na(sum(match(start, states)))) {
+      code <- paste(c(code, "start include:", start, "\n"), collapse = " ")
     }
     # if the starting beliefs are given by a uniform distribution over a subset of states (using their numbers)
     if (!is.character(start)) { 
       if (sum(start)>=1 & length(start)<number_of_states) {
-        code <- paste(c(code,"start include:",start, "\n"), collapse = " ")
+        code <- paste(c(code, "start include:", start, "\n"), collapse = " ")
       }
     }
   } else if (start[1]=="-") { ## if the starting beliefs exclude a specific subset of states
-    code <- paste(c(code,"start exclude:",start[-1], "\n"), collapse = " ")
+    code <- paste(c(code, "start exclude:", start[-1], "\n"), collapse = " ")
   }
   
   
@@ -90,7 +92,11 @@ pomdp <- function(discount = 0,
     }
     # writing the transition probabilitie lines
     for (i in 1:dim(transition_prob)[1]) {
-      code <- paste(c(code,"T:", as.character(transition_prob[i,1]), ":", as.character(transition_prob[i,2]), ":", as.character(transition_prob[i,3]), transition_prob[i,4],  "\n"), collapse = " ")
+      code <- paste(c(code,"T:", 
+        as.character(transition_prob[i,1]), ":", 
+        as.character(transition_prob[i,2]), ":", 
+        as.character(transition_prob[i,3]), 
+        transition_prob[i,4],  "\n"), collapse = " ")
     }
   }
   
@@ -104,15 +110,16 @@ pomdp <- function(discount = 0,
     # writing the transition probability matrices
     for (i in 1:number_of_actions) {
       code <- paste(c(code,"T:", actions[i], "\n"), collapse = " ")
-      if (any(transition_prob[[actions[i]]] == "uniform") | any(transition_prob[[actions[i]]] == "identity")) {
-        code <- paste(c(code, transition_prob[[actions[i]]] , "\n"), collapse = " ")
+      if (any(transition_prob[[actions[i]]] == "uniform") | 
+          any(transition_prob[[actions[i]]] == "identity")) {
+        code <- paste(c(code, transition_prob[[actions[i]]], "\n"), collapse = " ")
       } 
       else {
         c_m <- character()
         for (j in 1:number_of_states) {
-          c_m <- paste(c(c_m, transition_prob[[actions[i]]][j,], "\n"),collapse = " ")
+          c_m <- paste(c(c_m, transition_prob[[actions[i]]][j,], "\n"), collapse = " ")
         }
-        code <- paste(c(code,c_m), collapse = " ")
+        code <- paste(c(code, c_m), collapse = " ")
       }
     }
   }
@@ -128,7 +135,11 @@ pomdp <- function(discount = 0,
     }
     # writing the transition probabilities lines
     for (i in 1:dim(observation_prob)[1]) {
-      code <- paste(c(code,"O:", as.character(observation_prob[i,1]), ":", as.character(observation_prob[i,2]), ":", as.character(observation_prob[i,3]), observation_prob[i,4],  "\n"), collapse = " ")
+      code <- paste(c(code,"O:", 
+        as.character(observation_prob[i,1]), ":", 
+        as.character(observation_prob[i,2]), ":", 
+        as.character(observation_prob[i,3]), 
+        observation_prob[i,4],  "\n"), collapse = " ")
     }
   }
   
@@ -141,7 +152,8 @@ pomdp <- function(discount = 0,
     # writing the observation probability matrices
     for (i in 1:number_of_actions) {
       code <- paste(c(code,"O:", actions[i], "\n"), collapse = " ")
-      if (any(observation_prob[[actions[i]]] == "uniform") | any(observation_prob[[actions[i]]] == "identity")) {
+      if (any(observation_prob[[actions[i]]] == "uniform") | 
+          any(observation_prob[[actions[i]]] == "identity")) {
         code <- paste(c(code, observation_prob[[actions[i]]] , "\n"), collapse = " ")
       } 
       else {
@@ -149,7 +161,7 @@ pomdp <- function(discount = 0,
         for (j in 1:number_of_states) {
           c_m <- paste(c(c_m, observation_prob[[actions[i]]][j,], "\n"),collapse = " ")
         }
-        code <- paste(c(code,c_m), collapse = " ")
+        code <- paste(c(code, c_m), collapse = " ")
       }
     }
   }
@@ -164,7 +176,12 @@ pomdp <- function(discount = 0,
     }
     # writing the reward lines
     for (i in 1:dim(reward)[1]) {
-      code <- paste(c(code,"R:", as.character(reward[i,1]), ":", as.character(reward[i,2]), ":", as.character(reward[i,3]), ":", as.character(reward[i,4]), reward[i,5],  "\n"), collapse = " ")
+      code <- paste(c(code,"R:", 
+        as.character(reward[i,1]), ":", 
+        as.character(reward[i,2]), ":", 
+        as.character(reward[i,3]), ":", 
+        as.character(reward[i,4]), 
+        reward[i,5],  "\n"), collapse = " ")
     }
   }
   
@@ -173,38 +190,41 @@ pomdp <- function(discount = 0,
     # writing the reward section
     for (i in 1:number_of_actions) {
       for (j in 1:number_of_states) {
-        code <- paste(c(code, "R:" , actions[i] , ":" , states[j], "\n"))
-        c_m <- paste(rep(paste(c(rep(reward[i,j] , number_of_observations) , "\n") , collapse = " ") , number_of_states) , collapse = " ")
-        code <- paste(c(code,c_m),collapse = " ")
+        code <- paste(c(code, "R:" , actions[i], ":" , states[j], "\n"))
+        c_m <- paste(rep(paste(c(rep(reward[i,j], number_of_observations), "\n"),
+          collapse = " "), number_of_states), collapse = " ")
+        code <- paste(c(code, c_m),collapse = " ")
       }
     }
   }
   
   
   ### saving the POMDP file
-  pomdp_file <- tempfile( pattern = "model" , fileext = ".POMDP")
-  cat(code,file = pomdp_file)
+  pomdp_file <- tempfile(pattern = "model", fileext = ".POMDP")
+  cat(code, file = pomdp_file)
   
   ### running the POMDP code
   exec <- system.file("pomdp-solve", package="pomdp")
   shell_command <- sprintf("%s -pomdp %s -method grid -fg_points %d -fg_save true",
     exec, pomdp_file, grid_size)
-  solver_output <- system(shell_command, intern=TRUE,ignore.stdout = FALSE,ignore.stderr = FALSE,wait = TRUE)
+  solver_output <- system(shell_command, intern=TRUE,
+    ignore.stdout = FALSE, ignore.stderr = FALSE, wait = TRUE)
   
   ## the verbose mode: printing all the outputs from pomdp solver
   if(verbose) cat(paste(solver_output, "\n"))
   
   ### importing the outputs and results
-  file_id <- as.numeric(substr(solver_output[5],11,15))
-  result_files <- gsub(".POMDP" , "" , pomdp_file)
+  file_id <- as.numeric(substr(solver_output[5], 11, 15))
+  result_files <- gsub(".POMDP", "", pomdp_file)
   
   ## Creating result files' names and extensions
-  pg_filename <- sprintf("%s-%d.pg",result_files,file_id)
-  belief_filename <- sprintf("%s-%d.belief",result_files,file_id)
-  alpha_filename <- sprintf("%s-%d.alpha",result_files,file_id)
+  pg_filename <- sprintf("%s-%d.pg", result_files, file_id)
+  belief_filename <- sprintf("%s-%d.belief", result_files, file_id)
+  alpha_filename <- sprintf("%s-%d.alpha", result_files, file_id)
   
   ## importing pg file
-  pg <- read.table(pg_filename, header = FALSE, sep = " ", quote = "\"", dec = ".", na.strings = NA, numerals = "no.loss")
+  pg <- read.table(pg_filename, header = FALSE, sep = " ", quote = "\"", 
+    dec = ".", na.strings = NA, numerals = "no.loss")
   pg <- as.matrix(pg)
   pg <- pg[,c(-3,-dim(pg)[2])] #there 2 NA columns that need to be removed
   pg <- pg+1 #index has to start from 1 not 0
@@ -225,7 +245,7 @@ pomdp <- function(discount = 0,
   toDel <- seq(1,dim(alpha)[1],2)
   alpha <- alpha[-toDel,]
   alpha <- unlist(strsplit(alpha, " "))
-  alpha<- matrix(as.numeric(alpha),ncol = number_of_states, byrow = TRUE)
+  alpha<- matrix(as.numeric(alpha), ncol = number_of_states, byrow = TRUE)
   alpha_matrix <- alpha
   alpha <- as.data.frame(alpha)
   
@@ -245,7 +265,7 @@ pomdp <- function(discount = 0,
   ## modifying the alpha file
   # renaming the columns
   for (i in 1:number_of_states) {
-    colnames(alpha)[i] <- paste("coeffecient" , i)
+    colnames(alpha)[i] <- paste("coeffecient", i)
   }
   
   ## modifying the belief file
