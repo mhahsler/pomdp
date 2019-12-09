25a904f8998fb625f59f44d625c4c7d354814aa9
Michael Hahsler <michael@hahsler.net>
2019-12-09 01:53:38 -0600

    Moved pies to policy_graph().

--- R/plot.POMDP.R
+++ R/plot.POMDP.R
@@ -1,4 +1,4 @@
-policy_graph <- function(x) {
+policy_graph <- function(x, belief = TRUE, cols = NULL) {
   ## producing the optimal policy graph
   pg <- x$solution$pg
   
@@ -9,7 +9,7 @@ policy_graph <- function(x) {
   number_of_observations <- length(observations)
   l <- lapply(1:number_of_observations, FUN = function(i)
     data.frame(from = pg$node, to = pg[[observations[i]]], label = observations[i]))
-
+  
   l <- do.call(rbind, l)
   l <- l[!is.na(l$to),] # remove links to nowhere ('-' in pg)
   
@@ -20,13 +20,41 @@ policy_graph <- function(x) {
   policy_graph <- graph.edgelist(as.matrix(l[,1:2]))
   edge.attributes(policy_graph) <- list(label = l$label)
   edge.attributes(policy_graph) <- list(label = l$label)
-
+  
   ### Note: the space helps with moving the id away from the pie cut.
   init <- rep(":   ", nrow(x$solution$pg))
   init[x$solution$initial_belief_state] <- ": initial"
-   
+  
   V(policy_graph)$label <- paste0(x$solution$pg$node, init, 
     "\n", x$solution$pg$action) 
+  
+  belief <- belief && !is.null(x$solution$belief_proportions)
+  
+  if(belief) {
+    pie_values <- lapply(1:nrow(x$solution$belief_proportions), 
+      FUN = function(i) {
+        pv <- x$solution$belief_proportions[i,]
+        if(any(is.na(pv))) pv <- rep(1, length(pv)) else pv
+      })
+    
+    ### Set1 from Colorbrewer
+    number_of_states <- ncol(x$solution$belief_proportions)
+    if(is.null(cols)) {
+      cols <- c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33",
+        "#A65628", "#F781BF", "#999999")
+      if(number_of_states <= 9) cols <- cols[1:number_of_states]
+      else cols <- rainbow(number_of_states)
+    }else{
+      if(length(cols) != number_of_states) stop("Number of colors is not the number of states.")
+    }
+    
+    V(policy_graph)$shape <- "pie"
+    V(policy_graph)$pie = pie_values
+    V(policy_graph)$pie.color = list(cols)
+  }    
+  
+  V(policy_graph)$size <- 40 
+  E(policy_graph)$arrow.size  <- .5
   
   policy_graph
 }
@@ -56,50 +84,17 @@ policy_graph <- function(x) {
 
 
 
-plot.POMDP <- function(x, y = NULL, belief = TRUE, legend = TRUE, 
-  vertex.size = 40, edge.arrow.size =.5, cols = NULL, ...) {
+plot.POMDP <- function(x, y = NULL, belief = TRUE, legend = TRUE, cols = NULL, ...) {
   
-  policy_graph <- policy_graph(x)
-
-  belief <- belief && !is.null(x$solution$belief_proportions)
+  pg<- policy_graph(x, belief = belief, cols = cols)
+  plot.igraph(pg, ...)
   
-  if(belief) {
-    pie_values <- lapply(1:nrow(x$solution$belief_proportions), 
-      FUN = function(i) {
-        pv <- x$solution$belief_proportions[i,]
-        if(any(is.na(pv))) pv <- rep(1, length(pv)) else pv
-      })
-    
-    ### Set1 from Colorbrewer
-    number_of_states <- ncol(x$solution$belief_proportions)
-    if(is.null(cols)) {
-      cols <- c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33",
-        "#A65628", "#F781BF", "#999999")
-      if(number_of_states <= 9) cols <- cols[1:number_of_states]
-      else cols <- rainbow(number_of_states)
-    }else{
-      if(length(cols) != number_of_states) stop("Number of colors is not the number of states.")
-    }
-    
-    plot.igraph(policy_graph, 
-      vertex.shape = "pie", 
-      vertex.pie = pie_values,
-      vertex.pie.color = list(cols),
-      #edge.curved = curve_multiple_fixed(policy_graph),
-      vertex.size = vertex.size, edge.arrow.size = edge.arrow.size,
-      ...)
-  }else{  
-    plot.igraph(policy_graph, 
-      #edge.curved = curve_multiple_fixed(policy_graph),
-      vertex.size = vertex.size, edge.arrow.size = edge.arrow.size,
-      ...)
-  }
-  
-  if(legend && belief) {
+  if(legend && belief && !is.null(V(pg)$pie)) {
     legend("topright", legend = colnames(x$solution$belief_proportions), title = "Belief Proportions", 
       #horiz = TRUE,
       bty = "n",
-      col = cols, pch = 15
+      col = V(pg)$pie.color[[1]], 
+      pch = 15
     )
   }
 }
