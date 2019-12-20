e4a973036fb530605ec6f7807e726681286b1f1d
Michael Hahsler <michael@hahsler.net>
2019-12-20 17:10:00 -0600

    plot_value_function can now handle finite-horizon POMDPs.

--- R/plot_value_function.R
+++ R/plot_value_function.R
@@ -2,14 +2,24 @@
 
 ### FIXME: Add ternary plots for 3 states
 
-plot_value_function <- function(model, projection = 1:2, ylim = NULL, legend = TRUE) {
+plot_value_function <- function(model, projection = 1:2, epoch = 1, ylim = NULL, legend = TRUE) {
 
   .solved_POMDP(model)
 
   if(is.character(projection)) projection <- pmatch(projection, model$model$states)
   if(length(projection) != 2) stop("Value function needs to be projected onto two states for plotting.")
-    
-  alpha <- model$solution$alpha[,projection, drop = FALSE]
+  
+  alpha <- model$solution$alpha
+  pg <- model$solution$pg
+   
+  # finite-horizon
+  if(is.list(alpha)) {
+    if(epoch > length(alpha)) stop("The solution does not contain that many epochs. Either the horizon was set to less epochs or the solution converged earlier.")
+    alpha <- alpha[[epoch]]
+    pg <- pg[[epoch]]
+  }
+  
+  alpha <- alpha[,projection, drop = FALSE]
   if(is.null(ylim)) ylim <- c(min(alpha), max(alpha))
   
   plot(NA, xlim = c(0, 1), ylim = ylim, 
@@ -24,5 +34,5 @@ plot_value_function <- function(model, projection = 1:2, ylim = NULL, legend = T
   for(i in 1:nrow(alpha)) lines(x = c(0, 1), y = c(alpha[i,1], alpha[i,2]), col = i, xpd = FALSE)
   
   if(legend) legend("topright", legend = 
-      paste0(1:nrow(alpha),": ", model$solution$pg[,"action"]), col = 1:nrow(alpha), lwd=1, bty = "n")
+      paste0(1:nrow(alpha),": ", pg[,"action"]), col = 1:nrow(alpha), lwd=1, bty = "n")
 }
