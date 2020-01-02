3f6473c179614df45e29f20bdb17f882d31b67f1
Michael Hahsler <michael@hahsler.net>
2020-01-02 10:48:36 -0600

    Plot belief space figures out projection.

--- R/plot_belief_space.R
+++ R/plot_belief_space.R
@@ -1,9 +1,12 @@
 
 ### FIXME: Add belief points
-plot_belief_space <- function(model, projection = c(1,2), n = 100, 
+plot_belief_space <- function(model, projection = NULL, n = 100, 
   what = c("action", "pg_node", "reward"), legend = TRUE) {
+  
+  if(is.null(projection)) projection <- 1:min(length(model$model$states), 3)
+  
   p <- sample_belief_space(model, n = n, projection = projection)
-  alpha <- p$belief[,projection]
+  belief <- p$belief[,projection]
   
   what <- match.arg(what)
   val <- p$optimal[[what]]
@@ -13,21 +16,26 @@ plot_belief_space <- function(model, projection = c(1,2), n = 100,
   
   if(length(projection) == 3) {
     TernaryPlot(
-      alab=paste(colnames(alpha)[1], "\u2192"), 
-      blab=paste(colnames(alpha)[2], "\u2192"), 
-      clab=paste("\u2190", colnames(alpha)[3]), 
+      alab=paste(colnames(belief)[1], "\u2192"), 
+      blab=paste(colnames(belief)[2], "\u2192"), 
+      clab=paste("\u2190", colnames(belief)[3]), 
       grid.lines=10, grid.lty='dotted',
       grid.minor.lines=1, grid.minor.lty='dotted')  
     TernaryPoints(alpha, pch = 20, col = col)
+    
+    #FunctionToContour <- function (a, b, c) pomdp:::.rew(belief = cbind(a,b,c), model$solution$alpha[,projection])[,what]
+    #values <- Ternary::TernaryPointValues(FunctionToContour, resolution=128L)
+    #Ternary::ColourTernary(values)
+    #Ternary::TernaryContour(FunctionToContour, resolution=6L)
     
   } else if(length(projection) == 2) {
     plot(NA, 
       xlim = c(0,1), ylim = c(0,2),
       axes = FALSE, xlab = NA, ylab = NA)
     
-    points(alpha[,1], rep(0, times = nrow(alpha)), col = col, pch = 20) 
+    points(belief[,1], rep(0, times = nrow(belief)), col = col, pch = 20) 
     
-    axis(1, at = c(0,1), labels = colnames(alpha), xaxs = "i")
+    axis(1, at = c(0,1), labels = colnames(belief), xaxs = "i")
     axis(1, at = .5, .5) 
     
   } else stop("projection needs to be 2d or 3d.")
