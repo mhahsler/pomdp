2a8d87afd9a3a6a55ca80099bcb5a574aa5340d9
Michael Hahsler <michael@hahsler.net>
2022-12-19 10:42:39 -0600

    small changes.

--- R/AAA_pomdp-package.R
+++ R/AAA_pomdp-package.R
@@ -1,6 +1,5 @@
 #' @title `r packageDescription("pomdp")$Package`: `r packageDescription("pomdp")$Title`
 #'
-#'
 #' @description Provides the infrastructure to define and analyze the solutions of Partially Observable Markov Decision Process (POMDP) models. Interfaces for various exact and approximate solution algorithms are available including value iteration, Point-Based Value Iteration (PBVI) and Successive Approximations of the Reachable Space under Optimal Policies (SARSOP).
 #'
 #' @section Key functions:
