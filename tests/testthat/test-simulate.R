43f59e7ecf40cda4a9414e29f2647bd9ce526823
Michael Hahsler <mhahsler@smu.edu>
2024-02-06 14:29:57 -0600

    removed parallel backend for windows.

--- tests/testthat/test-simulate.R
+++ tests/testthat/test-simulate.R
@@ -7,7 +7,8 @@ verb <- FALSE
 #verb <- TRUE
 
 ### CRAN checker let's us use 2 processes
-doParallel::registerDoParallel(2)
+### but windows cannot do it!
+# doParallel::registerDoParallel(2)
 
 sim_c <- simulate_POMDP(Tiger, n = 1000, horizon = 5, return_beliefs = TRUE, verbose = verb)
 sim_r <- simulate_POMDP(Tiger, n = 1000, horizon = 5, return_beliefs = TRUE, verbose = verb, engine = 'r')
@@ -28,4 +29,5 @@ expect_lt(abs(mean(sim_c$reward) - mean(sim_r$reward)) / mean(sim_c$reward), 0.3
 problem_norm <- normalize_POMDP(problem, sparse = FALSE)
 system.time(simulate_POMDP(problem_norm, n = 100, horizon = 10, verbose = verb, engine = 'r'))
 
-doParallel::stopImplicitCluster()
+# deregister backend
+### foreach::registerDoSEQ()
