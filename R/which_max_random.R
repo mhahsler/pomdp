d596d23c14a80032547055b6679e0cc77cc935b3
Michael Hahsler <mhahsler@smu.edu>
2024-03-04 21:08:34 -0600

    deal with NAs.

--- R/which_max_random.R
+++ R/which_max_random.R
@@ -1,7 +1,9 @@
 # this is borrowed from nnet::which.is.max
 which.max.random <- function (x)
 {
-  y <- seq_along(x)[x == max(x)]
+  mx <- x == max(x, na.rm = TRUE)
+  mx[is.na(mx)] <- FALSE
+  y <- seq_along(x)[mx]
   if (length(y) > 1L)
     sample(y, 1L)
   else
